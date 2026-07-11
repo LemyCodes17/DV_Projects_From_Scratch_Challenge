9b7cdd9e5498cdb563c0154b70c8af45ddd9f9c2
Lemy Chineyemba <lemuel.chineyemba@olympus-login.engr.tamu.edu>
2026-07-10 22:37:42 -0500

    Initial commit

---
+++ Week-5(HTAX_sequencer)/sequencer.sv
@@ -0,0 +1,28 @@
+class htax_sequencer extends uvm_sequencer #(htax_sequence_item); //the HTAX packet sequencer class 
+ 
+`uvm_component_utils(htax_sequencer_c) //sequencer's are UVM component's, so they're registered as uvm_component_utils in the UVM factory db.
+
+function new (string name, uvm_component parent); // constructor function that declares two paprameters of data type string and uvm_component passes in 
+                                                  // uvm_component is a user declared parameter, while string us a ...
+                                                  // this is the standard function-pass structure of UVM component's all through.. Driver, Monitor etc. (pass in the name and uvm_component)
+    super.new(name, parent);                      // super.new calls uvm_sequencer's constructor which stores name, parent, and registers this component in UVM tree.
+                                                  // Skip this and UVM has no record that this component exists.
+endfunction
+                                                  // the sequencer class is set up for now...
+endclass: htax_packet_c
+
+
+//example sequencer 2
+class cpu_sequencer extends uvm_sequencer;
+
+`uvm_component_utils(cpu_sequencer)
+
+function new (string name, uvm_component parent);
+
+    super.new(name,parent);
+
+endfunction
+
+
+
+endclass
--- Week-5(HTAX_sequences_and_test)/sequencer.sv
+++
@@ -1,9 +0,0 @@
-class htax_packet_c extends uvm_sequencer;
-
-`uvm_component_utils(htax_sequencer_c)
-
-function new (string name, uvm_component parent);
-    super.new(name, parent);
-endfunction
-
-endclass: htax_packet_c
