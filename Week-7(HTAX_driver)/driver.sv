91ea77018de0c2faafd819cd6e81b508927a44ca
Lemy Chineyemba <lemuel.chineyemba@olympus-login.engr.tamu.edu>
2026-07-29 13:08:27 -0500

    Initial commit

--- Week-7(HTAX_driver)/driver.sv
+++ Week-7(HTAX_driver)/driver.sv
@@ -1,8 +1,8 @@
-class htax_driver_c extends uvm_driver(sequence_item);
+class htax_driver_c extends uvm_driver(sequence_item); // htax driver class extends uvm_driver oop fundermentals 
 
-`uvm_component_utils(htax_driver_c) //uvm macro registration
+`uvm_component_utils(htax_driver_c) // uvm component registration with uvm factory for: error reporting and task automation
 
-function new(string name = "htax_driver_c", parent); //htax_driver_c constructor
+function new(string name = "htax_driver_c", uvm_component parent); // htax driver class constructor  
 super.new(name,parent); 
 endfunction 
 
