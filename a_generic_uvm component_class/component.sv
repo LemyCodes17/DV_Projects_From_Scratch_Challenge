b660e2fa67b00bba468f45463003e84c42fad2ad
Lemy Chineyemba <lemuel.chineyemba@olympus-login.engr.tamu.edu>
2026-08-01 23:29:10 -0500

    Initial commit

---
+++ a_generic_uvm component_class/component.sv
@@ -0,0 +1,13 @@
+class my_component extends uvm_component;
+
+`uvm_component_utils(my_component)
+
+function new(string name, uvm_component parent);
+    super.new(name, parent);
+endfunction:constructor_func
+
+function build;
+functin run
+task;
+
+endclass
